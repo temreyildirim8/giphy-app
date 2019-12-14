e7f1ec249db694ff7ecb59d434457db9cc4f12fe
temreyildirim8 <emre.yildirim@fibabanka.com.tr>
2019-12-14 13:21:15 +0300

    results page compile correction

--- src/screens/Results/Results.js
+++ src/screens/Results/Results.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {   StyleSheet, Text, View } from 'react-native';
 
 export default class Results extends Component {
   render() {
@@ -15,7 +15,6 @@ export default class Results extends Component {
         <View style={styles.container}>
           <Text style={styles.welcome}>It is results page</Text>
           <Text style={styles.instructions}>It is results page</Text>
-          <Text style={styles.instructions}>{instructions}</Text>
         </View>
     );
   }
